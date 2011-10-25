122dcd05050ab910f12698b86754a4b6b447d6c8
Wojtek Kruszewski <wojtek@oxos.pl>
2011-10-25 22:23:04 +0200

    Add spec for making request on save button click

--- spec/javascripts/GadgetSpec.js
+++ spec/javascripts/GadgetSpec.js
@@ -2,6 +2,8 @@ describe("Configured gadget", function() {
   var gadget_window, getString;
 
   beforeEach(function() {
+    loadFixtures('gadget.html');
+
     window.google = {};
 
     var contentmatch = {getContentMatches: function(){} };
@@ -19,7 +21,23 @@ describe("Configured gadget", function() {
 
     window.gadgets = {
       Prefs: Prefs,
-      'window': gadget_window
+      'window': gadget_window,
+      io: {
+	makeRequest: jasmine.createSpy('makeRequest'),
+	ContentType: {
+	  DOM: 'DOM'
+	},
+	MethodType: {
+	  POST: 'POST',
+	  PUT: 'PUT'
+	},
+	RequestParameters: {
+	  METHOD: 'METHOD',
+	  HEADERS: 'HEADERS',
+	  CONTENT_TYPE: 'CONTENT_TYPE',
+	  POST_DATA: 'POST_DATA'
+	}
+      }
     };
 
     window.initializeGeepivoGadget();
@@ -32,5 +50,22 @@ describe("Configured gadget", function() {
   it("should adjust height to 32", function() {
     expect(getString).toHaveBeenCalled();
   });
+
+  it("should send xml when btn clicked", function() {
+    $('button.create_story_button').click();
+
+    expect(window.gadgets.io.makeRequest.callCount).toEqual(1);
+
+    var args = window.gadgets.io.makeRequest.mostRecentCall.args;
+    expect(args.shift()).toEqual('https://www.pivotaltracker.com/services/v3/projects/dupa/stories');
+    expect(typeof args.shift()).toEqual('function');
+    expect( args.shift() ).toEqual({
+      METHOD: 'POST',
+      HEADERS: { 'X-TrackerToken' : 'dupa', 'Content-type' : 'application/xml' },
+      CONTENT_TYPE: 'DOM',
+      POST_DATA: "<story>\n  <project_id>dupa</project_id>\n  <story_type>dupa</story_type>\n  <name>An email subject</name>\n  <integration_id>dupa</integration_id>\n  <requested_by>dupa</requested_by>\n  <owned_by>dupa</owned_by>\n</story>"
+    });
+  });
+
 
 });
