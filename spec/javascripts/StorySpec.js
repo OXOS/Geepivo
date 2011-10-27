810ac60aba913f383fd764706db457e9186a6683
Wojtek Kruszewski <wojtek@oxos.pl>
2011-10-27 22:21:17 +0200

    Make specs more "unit"

--- spec/javascripts/StorySpec.js
+++ spec/javascripts/StorySpec.js
@@ -18,16 +18,30 @@ describe("Story", function() {
 
   describe("_create_and_update_other_id", function() {
 
+    it("should make a request and call _response_callback method", function() {
+      var dummy_response = 1,
+        dummy_success_callback = 2,
+        dummy_error_callback = 3; 
+
+      spyOn(story.io,'makeRequest').andCallFake( function(url,callback,params){
+        callback( dummy_response );
+      } );
+      spyOn(story,'_response_callback');
+
+      story._create_and_update_other_id(dummy_success_callback, dummy_error_callback);
+
+      expect(story._response_callback).toHaveBeenCalledWith(dummy_response, dummy_success_callback, dummy_error_callback)
+    });
+  });
+
+  describe("_response_callback", function() {
+
     it("should call on_error callback with 'Error creating story' message", function() {
       on_success = jasmine.createSpy('on_success');
       on_error = jasmine.createSpy('on_error');
-      spyOn(story.io,'makeRequest').andCallFake( function(url,callback,params){
-        response = {
-          rc: 400
-          };
-        callback( response );
-      } );
-      story._create_and_update_other_id(on_success, on_error);
+      response = { rc: 400 };
+
+      story._response_callback( response, on_success, on_error );
 
       expect(on_success).not.toHaveBeenCalled();
       expect(on_error).toHaveBeenCalledWith("Error creating story");
