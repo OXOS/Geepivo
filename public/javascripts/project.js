405cd8ac310620df73d35292b8ac74ee119b92de
Wojtek Kruszewski <wojtek@oxos.pl>
2011-10-29 23:42:34 +0200

    Create Project class to get list of projects

---
+++ public/javascripts/project.js
@@ -0,0 +1,62 @@
+(function() {
+  var parse_xml;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  parse_xml = function(xml_string) {
+    var parser, respXML;
+    if (window.DOMParser) {
+      parser = new DOMParser();
+      respXML = parser.parseFromString(xml_string, "text/xml");
+    } else {
+      respXML = new ActiveXObject("Microsoft.XMLDOM");
+      respXML.async = "false";
+      respXML.loadXML(xml_string);
+    }
+    return respXML;
+  };
+  window.Project = (function() {
+    function Project(io) {
+      this.io = io;
+      this._get_index_callback = __bind(this._get_index_callback, this);
+    }
+    Project.prototype.get_index = function(on_success, on_error) {
+      var params, projects_url, response_callback;
+      projects_url = "https://www.pivotaltracker.com/services/v3/projects";
+      params = {};
+      params[this.io.RequestParameters.METHOD] = this.io.MethodType.GET;
+      params[this.io.RequestParameters.HEADERS] = {
+        "X-TrackerToken": this.pivotal_api_token,
+        "Content-type": "application/xml"
+      };
+      params[this.io.RequestParameters.CONTENT_TYPE] = this.io.ContentType.DOM;
+      response_callback = __bind(function(response) {
+        return this._get_index_callback(response, on_success, on_error);
+      }, this);
+      return this.io.makeRequest(projects_url, response_callback, params);
+    };
+    Project.prototype._get_index_callback = function(response, on_success, on_error) {
+      var message, projects_data, projects_dom, respXML;
+      if (response.rc >= 400) {
+        message = (function() {
+          switch (response.rc) {
+            case 401:
+              return "Authentication error - check your API token and project permissions";
+            default:
+              return null;
+          }
+        })();
+        return on_error(message);
+      } else {
+        respXML = parse_xml(response.text);
+        projects_dom = $(respXML).find("projects project");
+        projects_data = $.map(projects_dom, function(project, i) {
+          return {
+            id: parseInt($(project).children('id').text()),
+            name: $(project).children('name').text()
+          };
+        });
+        return on_success(projects_data);
+      }
+    };
+    return Project;
+  })();
+}).call(this);
--- spec/javascripts/helpers/GoogleGadgetsEnvironment.js
+++ spec/javascripts/helpers/GoogleGadgetsEnvironment.js
@@ -14,6 +14,7 @@ GoogleGadgetsEnvironmentStubs = function() {
     },
 
     MethodType: {
+      GET: 'GET',
       POST: 'POST',
       PUT: 'PUT'
     },
